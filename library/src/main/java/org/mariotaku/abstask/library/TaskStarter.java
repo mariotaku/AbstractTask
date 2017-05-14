870ea30a32de5d78e1f3e5042b82de391d832eb2
Mariotaku Lee <mariotaku.lee@gmail.com>
2017-05-14 21:48:30 +0800

    updated version

--- library/src/main/java/org/mariotaku/abstask/library/TaskStarter.java
+++ library/src/main/java/org/mariotaku/abstask/library/TaskStarter.java
@@ -1,22 +1,37 @@
 package org.mariotaku.abstask.library;
 
 import android.os.AsyncTask;
+import android.support.annotation.Nullable;
+
+import java.util.concurrent.Executor;
+import java.util.concurrent.atomic.AtomicReference;
 
 /**
  * Created by mariotaku on 16/2/24.
  */
 public class TaskStarter {
 
+    private static AtomicReference<Executor> sDefaultExecutor = new AtomicReference<>();
+
     public static <Params, Result, Callback> void execute(AbstractTask<Params, Result, Callback> task) {
         final AsyncTaskTask<Params, Result, Callback> asyncTaskTask = new AsyncTaskTask<>(task);
-        asyncTaskTask.execute();
+        Executor executor = sDefaultExecutor.get();
+        if (executor != null) {
+            asyncTaskTask.executeOnExecutor(executor);
+        } else {
+            asyncTaskTask.execute();
+        }
+    }
+
+    public static void setDefaultExecutor(@Nullable Executor executor) {
+        sDefaultExecutor.set(executor);
     }
 
     static class AsyncTaskTask<P, R, C> extends AsyncTask<Object, Object, R> {
 
         private final AbstractTask<P, R, C> task;
 
-        public AsyncTaskTask(AbstractTask<P, R, C> task) {
+        AsyncTaskTask(AbstractTask<P, R, C> task) {
             this.task = task;
         }
 
